1ffc4aac5beaa5be0d4b32dac425fff63cf7ee1f
Okpeyemi <maqsoud.tawaliou@epitech.eu>
2024-11-25 20:27:34 +0100

    feat: add the project font

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,14 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
+const LufgaMedium = localFont({
+  src: "./fonts/LufgaMedium.woff",
+  variable: "--font-lufga-medium",
   weight: "100 900",
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
+const LufgaRegular = localFont({
+  src: "./fonts/LufgaRegular.woff",
+  variable: "--font-lufga-regular",
   weight: "100 900",
 });
 
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${LufgaMedium.variable} ${LufgaRegular.variable} antialiased`}
       >
         {children}
       </body>
