1885af4ba203b652679e54f46c82b34bc0d407e2
Okpeyemi <maqsoud.tawaliou@epitech.eu>
2024-11-26 20:40:20 +0100

    feat: add new element

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,43 @@
-import NavBar from '@/components/Navbar/NavBar'
-import React from 'react'
+import Boutton from "@/components/Boutton";
+import NavBar from "@/components/Navbar/NavBar";
+import { Calendar } from "@/components/ui/calendar";
+import { CalendarDays } from "lucide-react";
+import React from "react";
 
 const page = () => {
   return (
     <div className="flex">
-        <NavBar />
-        <div>Ok</div>
+      <NavBar />
+      <div className="mx-5 my-5 w-full">
+        <h1>Welcome back, Maqsoud!</h1>
+        <h5 className="text-[#82828C]">
+          It is the best time to manage your finances
+        </h5>
+        <div className="flex justify-between">
+          <div className="flex my-5">
+            <div className="p-4 border-2 border-border w-fit rounded-full">
+              <CalendarDays />
+              {/* <Calendar
+              mode="single"
+              className="rounded-md border"
+            /> */}
+            </div>
+            <div className="p-4 border-2 border-border w-fit rounded-full">
+              <h6>This month</h6>
+            </div>
+          </div>
+          <div className="flex my-5 items-center">
+            <div className="p-4 border-2 border-border w-fit rounded-full mr-2">
+              <h6>This month</h6>
+            </div>
+            <Boutton 
+              text="Add new widget"
+            />
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
---
+++ components/Boutton.tsx
@@ -0,0 +1,28 @@
+import { Plus } from 'lucide-react';
+import React from 'react';
+
+type BouttonProps = {
+  text?: string;
+  Icon?: React.ElementType;
+  onClick?: () => void;
+  className?: string;
+};
+
+const Boutton: React.FC<BouttonProps> = ({
+  text,
+  Icon = Plus,
+  onClick,
+  className = '',
+}) => {
+  return (
+    <div
+      onClick={onClick}
+      className={`font-lufgaRegular flex items-center p-4 bg-primary text-white w-fit rounded-full cursor-pointer ${className}`}
+    >
+      <Icon />
+      {text}
+    </div>
+  );
+};
+
+export default Boutton;
